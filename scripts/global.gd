2f446723124c640e963979b8918fe6d634e5892d
mwasanje <noaheben@gmail.com>
2025-03-26 23:56:46 -0500

    Added quit when esc is pressed

--- scripts/global.gd
+++ scripts/global.gd
@@ -3,3 +3,7 @@ extends Node
 var water_level = 50
 var is_alive = true
 var max_water_level = 50
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
